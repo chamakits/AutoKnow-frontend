0fcea32c69420dfdda0c920150e4f8dbf2502eb7
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-01 22:27:02 -0400

    Created controller to create link with input fields. Will now use PureCSS.

--- app/links-create/links-create-ctrl.js
+++ app/links-create/links-create-ctrl.js
@@ -1,29 +1,42 @@
 (function() {
-  var URL = "http://localhost:8080/api/newlink"
+    var URL = "http://localhost:8080/api/newlink";
 
-  var autoKnowAppControllers = angular.module("autoKnowAppControllers");
+    var autoKnowAppControllers = angular.module("autoKnowAppControllers");
 
-  autoKnowAppControllers.factory("makelink", ["$resource", function($resource) {
-    return $resource(URL, {}, {
-      query: {
-        method: "POST",
-        params: {
-          "resourcePath": "myPath",
-          "redirectionPath": "http://www.google.com"
-        },
-        headers: {'Content-Type': 'application/json'}
-      }
-    });
-  }]);
-  autoKnowAppControllers.controller("LinkCreateCtrl", ["$scope", "makelink",
-    function(scope, makelink) {
-      scope.makeLinkFunction = function() {
-        var newLink = new makelink();
-        newLink.resourcePath = "myJsonPath";
-        newLink.redirectionPath = "https://duckduckgo.com/";
-        newLink.$save();
-      };
+    autoKnowAppControllers.factory("makelink", ["$resource", function($resource) {
+      return $resource(URL, {}, {
+        query: {
+          method: "POST",
+          params: {
+            "resourcePath": "myPath",
+            "redirectionPath": "http://www.google.com"
+          },
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      });
+    }]);
+    autoKnowAppControllers.controller("LinkCreateCtrl", ["$scope", "makelink",
+      function(scope, makelink) {
+        scope.makeLinkFunction = function() {
+          var newLink = new makelink();
+          newLink.resourcePath = "myJsonPath";
+          newLink.redirectionPath = "https://duckduckgo.com/";
+          newLink.$save();
+        };
 
-    }
-  ]);
+        console.log("Controller def.");
+
+        scope.create = function(link) {
+          console.log("Scope create");
+          console.log("link:" + link);
+          var newLink = new makelink();
+          newLink.resourcePath = link.resourcePath;
+          // newLink.redirectionPath = link.redirectionPath;
+          newLink.redirectionPath = link.redirectionPath;
+          newLink.$save();
+        };
+    
+    }]);
 }(this));
