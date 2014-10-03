408e07ec743a4b804b5695049e9bcd6bfbb08ddb
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-03 01:15:22 -0400

    Cleaned issue with beautifying. Added pre-commit hook.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,27 @@
 var gulp = require('gulp');
-var gulpBeautyWeb = require('./gulp-beauty-web.js').beautify;
+var gulpBeautyWeb = require('./utils/node/gulp-beauty-web.js').beautify;
 var beautify = require('js-beautify'); //.html;
 
 gulp.task("prettyallhtml", function() {
-  gulp.src('./app/**/*.html')
-    .pipe(gulpBeautyWeb.beautifyHtml(beautify.html))
+  gulp.src(['./app/**/*.html', '!./app/thirdparty/**'])
+    .pipe(gulpBeautyWeb(beautify.html))
     .pipe(gulp.dest('./app/'))
 })
 
 gulp.task("prettyalljs", function() {
-  gulp.src('./app/**/*.js')
+  gulp.src(['./app/**/*.js', '!./app/thirdparty/**'])
     .pipe(gulpBeautyWeb(beautify.js))
     .pipe(gulp.dest('./app/'))
 })
 
 gulp.task("prettyallcss", function() {
-  gulp.src('./app/**/*.css')
+  gulp.src(['./app/**/*.css', '!./app/thirdparty/**'])
     .pipe(gulpBeautyWeb(beautify.css))
     .pipe(gulp.dest('./app/'))
-  gulp.src('./style/**/*.css')
+  gulp.src(['./style/**/*.css', '!./style/thirdparty/**'])
     .pipe(gulpBeautyWeb(beautify.css))
-    .pipe(gulp.dest('./app/'))
+    .pipe(gulp.dest('./style/'))
 
 })
+
+gulp.task("prettyall", ["prettyallhtml", "pretyalljs", "prettyallcss"]);
