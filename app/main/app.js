e6ea4aedaaf4c672faeac6619a554b8221f785ac
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-05 13:23:45 -0400

    Changed html files from full html files to partials ('-part.html').

--- app/main/app.js
+++ app/main/app.js
@@ -2,6 +2,44 @@
     var autoKnowApp = angular.module("autoKnowApp", ["autoKnowAppControllers",
         "ngResource", "ngRoute"
     ]);
+
+    function MenuLinks(template, controller, link, text) {
+        this.template = template;
+        this.controller = controller;
+        this.link = link;
+        this.text = text;
+    };
+
     var autoKnowAppControllers = angular.module("autoKnowAppControllers", []);
+
+    // var autoKnowAppControllers = angular.module("autoKnowAppControllers");
+    var links = [
+        new MenuLinks("/app/main/home-sub.html", "", "/", "Home"),
+        new MenuLinks("/app/links-display/links-display-part.html",
+            "LinkDisplayCtrl", "/link-display", "Link Display"),
+        new MenuLinks("/app/links-create/links-create-part.html",
+            "LinkCreateCtrl", "/link-create", "Create link site"),
+        new MenuLinks("",
+            "", "localhost:8080/myPath", "myPath example"),
+    ];
+
+    autoKnowAppControllers.controller("MenuController", ["$scope", function($scope) {
+        $scope.headers = links;
+    }]);
+
+
+    autoKnowApp.config(["$routeProvider", function($routeProvider) {
+        var routeProvider = $routeProvider;
+        _(links).forEach(function(link) {
+            console.log("Setting links.");
+            routeProvider = routeProvider.when(link.link, {
+                templateUrl: link.template,
+                controller: link.controller
+            });
+        });
+        routeProvider.otherwise({
+            redirectTo: "/"
+        });
+    }]);
 
 }(this));
